7fdaf8d42b893e1eb6d399dfadfab0efbdba4b4f
EDWIN RUKUNO <erukuno@kabarak.ac.ke>
2024-01-22 19:48:56 +0300

    add and delete notes now

--- website/auth.py
+++ website/auth.py
@@ -21,7 +21,7 @@ def login():
                 flash('Incorrect password, try again.',category='error')
         else:
             flash('Email does not exist', category='error')            
-    return render_template("login.html")
+    return render_template("login.html",user=current_user)
 
 @auth.route('/logout')
 @login_required
@@ -56,4 +56,4 @@ def signup():
             flash("Account created!", category='success')
             return redirect(url_for('views.home'))
 
-    return render_template("signup.html")
+    return render_template("signup.html", user=current_user)
